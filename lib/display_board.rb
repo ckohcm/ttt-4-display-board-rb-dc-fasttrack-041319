537d7a06ca554bdc64eedfc97bd12ee47815093d
Catherine <catkcm.16@gmail.com>
2019-04-14 13:19:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+<<<<<<< HEAD
 # Building the board
 
 # def display_board
+=======
+# def display_board(board)
+>>>>>>> 264986ff9f8fb9e10b34404bd3984ff915474aed
 #   puts "   |   |   "
 #   puts "-----------"
 #   puts "   |   |   "
@@ -11,17 +15,21 @@
 #   puts "   |   |   "
 # end
 
+<<<<<<< HEAD
 # print display_board
 
 n = ["X", " ", "O", " ", "X", " ", " ", " ", "X"]
 # puts board
 
+=======
+>>>>>>> 264986ff9f8fb9e10b34404bd3984ff915474aed
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+<<<<<<< HEAD
 end
 
 display_board(n)
@@ -33,4 +41,16 @@ display_board(n)
 # end
 
 # print board
+=======
+  d
+
+
+board = []
+ 
+9.times do
+ board.push(" ")
+end
+
+print board
+>>>>>>> 264986ff9f8fb9e10b34404bd3984ff915474aed
 
